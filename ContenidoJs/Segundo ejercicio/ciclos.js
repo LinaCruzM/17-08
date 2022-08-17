4afccdcf77d82631b2d145669ea7f27b367bbf9c
LinaCruzM <109240037+LinaCruzM@users.noreply.github.com>
2022-08-17 12:59:01 -0500

    Add files via upload

---
+++ ContenidoJs/Segundo ejercicio/ciclos.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * 1 Ciclo Para = For
+ * 2 Ciclo Mientras = While
+ * 3 Repetir hasta = Do while
+ */
+
+//1
+for(let i=0; i<10; i++){
+    console.log(i);
+}
---
+++ ContenidoJs/Segundo ejercicio/condicionales.js
@@ -0,0 +1,18 @@
+'use strict';
+/**
+ * Condicionales
+ */
+
+let num1 = -8;
+
+if (num1 > 0){
+    console.log('Positivo');
+}else{
+    if (num1 == 0){
+        console.log('Cero');
+
+    }else{
+        console.log('Negativo');
+
+    }
+}
